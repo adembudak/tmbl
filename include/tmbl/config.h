f5487ea8d72a9537f9cec2179c981b448bd444b0
Adem Budak <admbudk@gmail.com>
2020-05-25 20:53:33 -0400

    Add a type trait to check whether type is Regular

--- include/tmbl/config.h
+++ include/tmbl/config.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <cstdint>
+#include <type_traits>
 
 namespace tmbl {
-using reg8 = std::uint8_t;
-using reg16 = std::uint16_t;
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
 using byte = char;
+using flag = const bool;
+
+template <typename T>
+struct is_regular
+    : std::integral_constant<bool, std::is_default_constructible_v<T> &&
+                                       std::is_copy_constructible_v<T> &&
+                                       std::is_move_constructible_v<T> &&
+                                       std::is_copy_assignable_v<T> &&
+                                       std::is_move_assignable_v<T>> {};
+
+template <typename T>
+static constexpr const bool is_regular_v = is_regular<T>::value;
+
 }
