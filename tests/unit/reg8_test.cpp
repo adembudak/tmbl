f679ae3f79fb84b023e17ba6ebf7b6fe8c3def68
Adem Budak <admbudk@gmail.com>
2020-05-27 18:56:29 -0400

    Added: Tests for reg8 type

--- tests/unit/reg8_test.cpp
+++ tests/unit/reg8_test.cpp
@@ -1,10 +1,13 @@
-#include <gtest/gtest.h>
 #include "tmbl/cpu/registers/reg8.h"
+#include "tmbl/cpu/registers/reg16.h"
+#include <tmbl/config.h>
 
+#include <gtest/gtest.h>
+
+using namespace tmbl;
 using namespace tmbl::cpu;
 
 TEST(EightBitRegisterType, Construction) {
-
   reg8 A;
 
   tmbl::uint8 val = 0b0000'0000;
@@ -22,3 +25,72 @@ TEST(EightBitRegisterType, Construction) {
   ASSERT_EQ(C, B);
 }
 
+TEST(EightBitRegisterType, ZeroFlag) {
+  reg8 A;
+
+  A.Z();
+  ASSERT_EQ(A, 0b1000'0000);
+
+  A.Z(false);
+  ASSERT_EQ(A, 0b0000'0000);
+}
+
+TEST(EightBitRegisterType, NegationFlag) {
+  reg8 A;
+
+  A.N();
+  ASSERT_EQ(A, 0b0100'0000);
+
+  A.N(true);
+  ASSERT_EQ(A, 0b0100'0000);
+
+  A.N(false);
+  ASSERT_EQ(A, 0b0000'0000);
+}
+
+TEST(EightBitRegisterType, BorrowingFlag) {
+  reg8 A;
+
+  A.H();
+  ASSERT_EQ(A, 0b0010'0000);
+}
+
+TEST(EightBitRegisterType, CarryFlag) {
+  reg8 A;
+
+  A.C();
+  ASSERT_EQ(A, 0b0001'0000);
+
+  A.C(false);
+  ASSERT_EQ(A, 0b0000'0000);
+}
+
+TEST(EightBitRegisterType, Allofthem) {
+  reg8 A;
+
+  A.Z();
+  A.N();
+  A.H();
+  A.C();
+
+  auto data = A.data();
+  ASSERT_EQ(data, 0b1111'0000);
+}
+
+TEST(EightBitRegisterType, AssignReg8toAnIntermediateValue) {
+  reg8 A;
+
+  A = 0b0000'1010;
+  ASSERT_EQ(A, 0b0000'1010);
+}
+
+TEST(EightBitRegisterType, AssignReg8toReg16) {
+  uint16 val = 0b0110'0000'1010'0011;
+
+  reg16 BC;
+  BC = val;
+
+  reg8 A;
+  A = BC;
+  ASSERT_EQ(A, 0b1010'0011);
+}
