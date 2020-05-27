9089b255675f06a91a93d73d6d29c5331f342448
Adem Budak <admbudk@gmail.com>
2020-05-26 20:09:37 -0400

    Add implementation file for Gameboy

---
+++ include/tmbl/Gameboy.cpp
@@ -0,0 +1,12 @@
+#include "Gameboy.hpp"
+
+namespace tmbl {
+
+// TODO: implement those functions
+
+void Gameboy::saveState() {}
+void Gameboy::loadState() {}
+void Gameboy::pause() {}
+void Gameboy::run() {}
+
+}
