08fa527befc6d774f88ea6331c9174225ef7e5be
Adem Budak <admbudk@gmail.com>
2020-05-27 19:44:01 -0400

    Use std::byte instead of char for memory block Mark constant mask values with constexpr

--- include/tmbl/config.h
+++ include/tmbl/config.h
@@ -3,12 +3,13 @@
 
 #include <boost/safe_numerics/safe_integer.hpp>
 #include <cstdint>
+#include <cstddef>
 #include <type_traits>
 
 namespace tmbl {
 using uint8 = boost::safe_numerics::safe<std::uint8_t>;
 using uint16 = boost::safe_numerics::safe<std::uint16_t>;
-using byte = boost::safe_numerics::safe<char>;
+using byte = boost::safe_numerics::safe<std::byte>;
 using flag = boost::safe_numerics::safe<const bool>;
 
 /*
@@ -18,8 +19,8 @@ using byte = char;
 using flag = const bool;
 */
 
-const uint16 zeroed_upper_byte_mask = 0b0000'0000'1111'1111;
-const uint16 zeroed_lower_byte_mask = 0b1111'1111'0000'0000;
+constexpr uint16 zeroed_upper_byte_mask = 0b0000'0000'1111'1111;
+constexpr uint16 zeroed_lower_byte_mask = 0b1111'1111'0000'0000;
 
 template <typename T>
 struct is_regular
