be8007530de7b2f805a7f8f79c5d915e4b488cfa
Adem Budak <admbudk@gmail.com>
2020-05-30 02:36:32 +0300

    comment out the safe numerics for a while

--- include/tmbl/config.h
+++ include/tmbl/config.h
@@ -7,17 +7,17 @@
 #include <type_traits>
 
 namespace tmbl {
+/*
 using uint8 = boost::safe_numerics::safe<std::uint8_t>;
 using uint16 = boost::safe_numerics::safe<std::uint16_t>;
 using byte = boost::safe_numerics::safe<std::byte>;
 using flag = boost::safe_numerics::safe<const bool>;
+*/
 
-/*
 using uint8 = std::uint8_t;
 using uint16 = std::uint16_t;
 using byte = char;
 using flag = const bool;
-*/
 
 constexpr uint16 zeroed_upper_byte_mask = 0b0000'0000'1111'1111;
 constexpr uint16 zeroed_lower_byte_mask = 0b1111'1111'0000'0000;
