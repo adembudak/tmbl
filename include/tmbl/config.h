cbcf67a2f7aad343546aafd26c21760eb26fee53
Adem Budak <admbudk@gmail.com>
2020-06-02 19:06:21 +0300

    - Rename uint8 and uint16 type definitions to u8 and u16. - Move zeroed_upper_byte_mask and zeroed_lower_byte_mask variables to reg16 class which it's only used.

--- include/tmbl/config.h
+++ include/tmbl/config.h
@@ -2,25 +2,22 @@
 #define CONFIG_H
 
 #include <boost/safe_numerics/safe_integer.hpp>
+
 #include <cstdint>
 #include <cstddef>
-#include <type_traits>
 
 namespace tmbl {
-/*
-using uint8 = boost::safe_numerics::safe<std::uint8_t>;
-using uint16 = boost::safe_numerics::safe<std::uint16_t>;
+using u8 = boost::safe_numerics::safe<std::uint8_t>;
+using u16 = boost::safe_numerics::safe<std::uint16_t>;
 using byte = boost::safe_numerics::safe<std::byte>;
 using flag = boost::safe_numerics::safe<const bool>;
-*/
 
-using uint8 = std::uint8_t;
-using uint16 = std::uint16_t;
+/*
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
 using byte = char;
 using flag = const bool;
-
-constexpr uint16 zeroed_upper_byte_mask = 0b0000'0000'1111'1111;
-constexpr uint16 zeroed_lower_byte_mask = 0b1111'1111'0000'0000;
+*/
 
 template <typename T>
 struct is_regular
