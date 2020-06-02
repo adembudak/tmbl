6d1693974f42f79e8ac15114b004563dccce3378
Adem Budak <admbudk@gmail.com>
2020-06-02 21:50:30 +0300

    The operator[] overloaded for cpu::reg8,cpu::reg16 and u8, u16 types.
    Most of instructions read/write to memory by indexing these types.

--- include/tmbl/memory/memory.cpp
+++ include/tmbl/memory/memory.cpp
@@ -1,8 +1,17 @@
 #include "memory.h"
+#include "../cpu/registers/reg16.h"
+#include "../cpu/registers/reg8.h"
+#include "../config.h"
 
 namespace tmbl::memory {
 
 [[nodiscard]] bool memory::dumpROM(const char *romfile) { return {}; }
 [[nodiscard]] byte *memory::data() const noexcept { return ram.data(); }
+
+[[nodiscard]] byte &memory::operator[](cpu::reg8 r) { return ram.at(r.data()); }
+[[nodiscard]] byte &memory::operator[](cpu::reg16 rr) { return ram.at(rr.data()); }
+
+[[nodiscard]] byte &memory::operator[](u8 n) { return ram.at(n); }
+[[nodiscard]] byte &memory::operator[](u16 nn) { return ram.at(nn); }
 
 }
--- include/tmbl/memory/memory.h
+++ include/tmbl/memory/memory.h
@@ -2,6 +2,9 @@
 #define MEMORY_H
 
 #include "../config.h"
+#include "../cpu/registers/reg16.h"
+#include "../cpu/registers/reg8.h"
+#
 #include <array>
 
 namespace tmbl::memory {
@@ -10,6 +13,13 @@ class memory final {
 public:
   [[nodiscard]] bool dumpROM(const char *romfile);
   [[nodiscard]] byte *data() const noexcept;
+
+  [[nodiscard]] byte &operator[](std::size_t index);
+  [[nodiscard]] byte &operator[](cpu::reg8 r);
+  [[nodiscard]] byte &operator[](cpu::reg16 rr);
+
+  [[nodiscard]] byte &operator[](u8 n);
+  [[nodiscard]] byte &operator[](u16 nn);
 
 private:
   mutable std::array<byte, 64 * 1024> ram{};
