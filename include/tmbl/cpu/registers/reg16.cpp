58fbff7bb109f46006336d1a0a66bf2acf46c689
Adem Budak <admbudk@gmail.com>
2020-06-05 17:03:49 +0300

    Added: lo(const byte b) and hi(const byte b) functions to load byte to registers from memory.
    Added: min() and max() static member functions.

--- include/tmbl/cpu/registers/reg16.cpp
+++ include/tmbl/cpu/registers/reg16.cpp
@@ -17,18 +17,21 @@ reg16 &reg16::operator-=(const u8 n) {
   return *this;
 }
 
-byte reg16::lo() const noexcept {
-  return static_cast<byte>(m_data & reg16::zeroed_upper_byte_mask);
+byte reg16::lo() const noexcept { return byte(m_data & reg16::zeroed_upper_byte_mask); }
+byte reg16::hi() const noexcept {
+  return byte((m_data & reg16::zeroed_lower_byte_mask) >> 8);
 }
 
-byte reg16::hi() const noexcept {
-  return static_cast<byte>((m_data & reg16::zeroed_lower_byte_mask) >> 8);
+void reg16::lo(const byte b) {
+  m_data = (m_data & zeroed_lower_byte_mask) | std::to_integer<int>(b);
+}
+
+void reg16::hi(const byte b) {
+  m_data = ((m_data & zeroed_upper_byte_mask) | std::to_integer<int>(b)) << 8;
 }
 
 [[nodiscard]] u16 reg16::data() const noexcept { return m_data; }
-
 [[nodiscard]] u16 operator-(const reg16 rr, const int i) { return rr.data() - i; }
-
 [[nodiscard]] u16 operator+(const reg16 rr, const int i) { return rr.data() + i; }
 
 }
--- include/tmbl/cpu/registers/reg16.h
+++ include/tmbl/cpu/registers/reg16.h
@@ -14,10 +14,19 @@ public:
   byte lo() const noexcept;
   byte hi() const noexcept;
 
+  void lo(const byte b);
+  void hi(const byte b);
+
+  static constexpr u16 min() noexcept { return min_u16_val; }
+  static constexpr u16 max() noexcept { return max_u16_val; }
+
   [[nodiscard]] u16 data() const noexcept;
 
 private:
   mutable u16 m_data = 0b0000'0000'0000'0000;
+
+  static constexpr u16 min_u16_val = 0b0000'0000'0000'0000;
+  static constexpr u16 max_u16_val = 0b1111'1111'1111'1111;
 
   static constexpr u16 zeroed_upper_byte_mask = 0b0000'0000'1111'1111;
   static constexpr u16 zeroed_lower_byte_mask = 0b1111'1111'0000'0000;
