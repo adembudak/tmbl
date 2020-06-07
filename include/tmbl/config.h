90b50ff6effdd8d511b23273d3747fdf6b6bacea
Adem Budak <admbudk@gmail.com>
2020-06-07 20:45:51 +0300

    Change std::uint8_t and std::uint16 types with their 'fast' counterparts
    I wonder what are the fast integer types a useful for and make search
    about them. Then saw is StackOverflow* question, and had conclusion that
    these are exactly what I'm looking for because u8 and u16 types
    definitions represent internal data of the registers (reg8 and reg16) of
    the cpu. Because registers suppose to fast:smile:, I changed the
    corresponding changes.
    
    https://stackoverflow.com/questions/46446453/what-is-a-fast-integer-what-defines-how-fast-an-integer-is

--- include/tmbl/config.h
+++ include/tmbl/config.h
@@ -7,8 +7,8 @@
 
 namespace tmbl {
 
-using u8 = boost::safe_numerics::safe<std::uint8_t>;
-using u16 = boost::safe_numerics::safe<std::uint16_t>;
+using u8 = boost::safe_numerics::safe<std::uint_fast8_t>;
+using u16 = boost::safe_numerics::safe<std::uint_fast16_t>;
 using byte = boost::safe_numerics::safe<std::byte>;
 using flag = boost::safe_numerics::safe<const bool>;
 
