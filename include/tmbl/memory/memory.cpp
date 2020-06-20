25975b90e77c63ba6d99e4fa41f0e93af5100042
Adem Budak <admbudk@gmail.com>
2020-06-20 19:59:24 +0300

    Remove operator[] to reach memory.
    This is done by the bus.
    
    Put nintendo_logo to ram.

--- include/tmbl/memory/memory.cpp
+++ include/tmbl/memory/memory.cpp
@@ -2,15 +2,15 @@
 #include "../cpu/registers/reg16.h"
 #include "../cpu/registers/reg8.h"
 #include "../config.h"
+#include <algorithm>
 
 namespace tmbl::memory {
+memory::memory() {
+  std::copy(std::begin(nintendo_logo), std::end(nintendo_logo), std::data(m_data) + 0x104);
+}
 
 [[nodiscard]] bool memory::dumpROM(const char *romfile) { return {}; }
 [[nodiscard]] byte *memory::data() const noexcept { return m_data.data(); }
 
-[[nodiscard]] byte &memory::operator[](const cpu::reg8 r) { return m_data.at(r.value()); }
-[[nodiscard]] byte &memory::operator[](const cpu::reg16 rr) { return m_data.at(rr.value()); }
-
-[[nodiscard]] byte &memory::operator[](const u8 n) { return m_data.at(n); }
 [[nodiscard]] byte &memory::operator[](const u16 nn) { return m_data.at(nn); }
 }
