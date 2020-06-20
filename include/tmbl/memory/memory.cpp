0e3d04f20a91ad0721d7523a0a353ac72c01a67f
Adem Budak <admbudk@gmail.com>
2020-06-20 20:07:11 +0300

    Pinpoint memory registers.

--- include/tmbl/memory/memory.cpp
+++ include/tmbl/memory/memory.cpp
@@ -13,4 +13,46 @@ memory::memory() {
 [[nodiscard]] byte *memory::data() const noexcept { return m_data.data(); }
 
 [[nodiscard]] byte &memory::operator[](const u16 nn) { return m_data.at(nn); }
+
+byte &memory::IP() noexcept { return m_data[0xFF00]; }
+byte &memory::SB() noexcept { return m_data[0xFF01]; }
+byte &memory::SC() noexcept { return m_data[0xFF02]; }
+byte &memory::DIV() noexcept { return m_data[0xFF04]; }
+byte &memory::TIMA() noexcept { return m_data[0xFF05]; }
+byte &memory::TMA() noexcept { return m_data[0xFF06]; }
+byte &memory::TAC() noexcept { return m_data[0xFF07]; }
+
+// bank kontrol registers -CGB only
+byte &memory::KEY1() noexcept { return m_data[0xFF4D]; }
+byte &memory::RP() noexcept { return m_data[0xFF56]; }
+
+// Interrupt flags
+byte &memory::IF() noexcept { return m_data[0xFF0F]; }
+byte &memory::IE() noexcept { return m_data[0xFFFF]; }
+
+// display registers
+byte &memory::LCDC() noexcept { return m_data[0xFF40]; }
+byte &memory::STAT() noexcept { return m_data[0xFF41]; }
+byte &memory::SCY() noexcept { return m_data[0xFF42]; }
+byte &memory::SCX() noexcept { return m_data[0xFF43]; }
+byte &memory::LY() noexcept { return m_data[0xFF44]; }
+byte &memory::LYC() noexcept { return m_data[0xFF45]; }
+byte &memory::DMA() noexcept { return m_data[0xFF46]; }
+byte &memory::BGP() noexcept { return m_data[0xFF47]; }
+byte &memory::OBP0() noexcept { return m_data[0xFF48]; }
+byte &memory::OBP1() noexcept { return m_data[0xFF49]; }
+byte &memory::WY() noexcept { return m_data[0xFF4A]; }
+byte &memory::WX() noexcept { return m_data[0xFF4B]; }
+
+// display registers -  CGB only
+byte &memory::HDMA1() noexcept { return m_data[0xFF51]; }
+byte &memory::HDMA2() noexcept { return m_data[0xFF52]; }
+byte &memory::HDMA3() noexcept { return m_data[0xFF53]; }
+byte &memory::HDMA4() noexcept { return m_data[0xFF54]; }
+byte &memory::HDMA5() noexcept { return m_data[0xFF55]; }
+byte &memory::BCPS() noexcept { return m_data[0xFF68]; }
+byte &memory::BCPD() noexcept { return m_data[0xFF69]; }
+byte &memory::OCPS() noexcept { return m_data[0xFF6A]; }
+byte &memory::OCPD() noexcept { return m_data[0xFF6B]; }
+
 }
--- include/tmbl/memory/memory.h
+++ include/tmbl/memory/memory.h
@@ -19,6 +19,48 @@ public:
 
   [[nodiscard]] byte &operator[](const u16 nn);
 
+  // port mode registers
+  byte &IP() noexcept;
+  byte &SB() noexcept;
+  byte &SC() noexcept;
+  byte &DIV() noexcept;
+  byte &TIMA() noexcept;
+  byte &TMA() noexcept;
+  byte &TAC() noexcept;
+
+  // bank control registers -CGB only
+  byte &KEY1() noexcept;
+  byte &RP() noexcept;
+
+  // Interrupt flags
+  byte &IF() noexcept;
+  byte &IE() noexcept;
+
+  // display registers
+  byte &LCDC() noexcept;
+  byte &STAT() noexcept;
+  byte &SCY() noexcept;
+  byte &SCX() noexcept;
+  byte &LY() noexcept;
+  byte &LYC() noexcept;
+  byte &DMA() noexcept;
+  byte &BGP() noexcept;
+  byte &OBP0() noexcept;
+  byte &OBP1() noexcept;
+  byte &WY() noexcept;
+  byte &WX() noexcept;
+
+  // display re
+  byte &HDMA1() noexcept;
+  byte &HDMA2() noexcept;
+  byte &HDMA3() noexcept;
+  byte &HDMA4() noexcept;
+  byte &HDMA5() noexcept;
+  byte &BCPS() noexcept;
+  byte &BCPD() noexcept;
+  byte &OCPS() noexcept;
+  byte &OCPD() noexcept;
+
 private:
   const byte nintendo_logo[48]{0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73,
                                0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F,
