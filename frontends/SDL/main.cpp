34acbad7ca2cd9e29d9d493efb2d6aa3f7612fb9
Adem Budak <admbudk@gmail.com>
2020-06-22 22:00:04 +0300

    Bake sdl front end a little bit more.

--- frontends/SDL/main.cpp
+++ frontends/SDL/main.cpp
@@ -1,19 +1,93 @@
 #include "tmbl/Gameboy.hpp"
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_video.h>
+#include <iostream>
 
-#include <string>
+void echo(const char *message) { std::cout << message << '\n'; }
 
-int main(int argc, char *argv[]) {
+int main(int /*argc*/, char * /*argv*/[]) {
+  tmbl::Gameboy machine;
 
   SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 
-  std::string title = "gametitle here";
+  SDL_Window *window = SDL_CreateWindow(/*title=*/"Something", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, 160 * 5, 144 * 5, SDL_WINDOW_SHOWN);
 
-  SDL_Window *window =
-      SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160, 144,
-                       SDL_WINDOW_SHOWN | SDL_WINDOW_BORDERLESS);
+  SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
+  SDL_bool done = SDL_FALSE;
+  SDL_Event event;
+
+  while (!done) {
+    while (!done && SDL_PollEvent(&event)) {
+      switch (event.type) {
+
+        case SDL_QUIT:
+          done = SDL_TRUE;
+          break;
+
+        case SDL_DROPFILE:
+          char *droppped_file = event.drop.file;
+
+          machine.init(droppped_file);
+
+          SDL_free(droppped_file);
+          break;
+      }
+
+      switch (event.key.state) {
+
+        case SDL_PRESSED:
+          switch (event.key.keysym.sym) {
+
+              // TODO implement controller
+            case SDLK_a:
+              [[fallthrough]];
+            case SDLK_LEFT:
+              echo("A or left pressed");
+              break;
+
+            case SDLK_w:
+              [[fallthrough]];
+            case SDLK_UP:
+              echo("w or up pressed");
+              break;
+
+            case SDLK_d:
+              [[fallthrough]];
+            case SDLK_RIGHT:
+              echo("d or right pressed");
+              break;
+
+            case SDLK_s:
+              [[fallthrough]];
+            case SDLK_DOWN:
+              echo("s or down pressed");
+              break;
+
+            case SDLK_SPACE:
+              echo("space pressed");
+              break;
+              break;
+
+            case SDLK_ESCAPE:
+              echo("esc pressed");
+              break;
+          }
+
+        case SDL_RELEASED:
+          switch (event.key.keysym.sym) {
+
+              // TODO implement controller
+            case SDLK_a:
+              [[fallthrough]];
+            case SDLK_LEFT:
+              echo("A or left released");
+              break;
+          }
+      }
+    }
+    //    SDL_Delay(0);
+  }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
