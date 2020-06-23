a924828ee03af172c6e9d814559a9918f7e34e79
Adem Budak <admbudk@gmail.com>
2020-06-24 01:19:02 +0300

    Restructure cartridge ... or... move the shit around....

--- include/tmbl/cartridge/cartridge.cpp
+++ include/tmbl/cartridge/cartridge.cpp
@@ -2,38 +2,187 @@
 
 #include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <memory>
 #include <numeric>
 
 namespace tmbl::cartridge {
 
-void cartridge::load(const std::filesystem::path &p) {
+enum cartridge_type {
+  ROM_ONLY = 0x00,
+
+  ROM_MBC1 = 0x01,
+  ROM_MBC1_RAM = 0x02,
+  ROM_MBC1_RAM_BATTERY = 0x03,
+
+  ROM_MBC2 = 0x05,
+  ROM_MBC2_RAM_BATTERY = 0x06,
+
+  ROM_RAM = 0x08,
+  ROM_RAM_BATTERY = 0x09,
+
+  ROM_MMM01 = 0x0B,
+  ROM_MMM01_SRAM = 0x0C,
+  ROM_MMM01_SRAM_BATTERY = 0x0D,
+
+  ROM_MBC3_TIMER_BATTERY = 0x0F,
+  ROM_MBC3_TIMER_RAM_BATTERY = 0x10,
+  ROM_MBC3 = 0x11,
+  ROM_MBC3_RAM = 0x12,
+  ROM_MBC3_RAM_BATTERY = 0x13,
+
+  ROM_MBC5 = 0x19,
+  ROM_MBC5_RAM = 0x1A,
+  ROM_MBC5_RAM_BATTERY = 0x1B,
+
+  ROM_MBC5_RUMBLE = 0x1C,
+  ROM_MBC5_RUMBLE_SRAM = 0x1D,
+  ROM_MBC5_RUMBLE_SRAM_BATTERY = 0x1E,
+
+  ROM_MBC6_RAM_BATTERY = 0x20,
+  ROM_MBC7_RAM_BATTERY_ACCELEROMETER = 0x22,
+
+  POCKET_CAMERA = 0xFC,
+  BANDAI_TAMA5 = 0xFD,
+
+  Hudson_HuC_3 = 0xFE,
+  Hudson_HuC_1 = 0xFF
+};
+
+enum cartridge_rom {
+  ROM_KB_32 = 0x00,
+  ROM_KB_64 = 0x01,
+  ROM_KB_128 = 0x02,
+  ROM_KB_256 = 0x03,
+  ROM_KB_512 = 0x04,
+  ROM_KB_1024 = 0x05,
+  ROM_KB_2048 = 0x06,
+  ROM_KB_4096 = 0x07,
+  ROM_KB_8192 = 0x08,
+
+  ROM_KB_1152 = 0x52,
+  ROM_KB_1280 = 0x53,
+  ROM_KB_1526 = 0x54
+};
+
+enum cartridge_ram {
+  RAM_None = 0x00,
+  RAM_KB_2 = 0x01,
+  RAM_KB_8 = 0x02,
+  RAM_KB_32 = 0x03,
+  RAM_KB_128 = 0x04,
+  RAM_KB_64 = 0x05
+};
+
+enum class gameboy_type {
+  DMG, // Dot matrix Game Boy, 1989
+  MGB, // Game Boy Pocket, 1996
+  MGL, // Game Boy Light, 1998
+  CGB, // Game Boy Color, 1998
+  SGB, // Super Game Boy, 1994,
+  SGB2 // Super Game Boy 2, 1998
+};
+
+cartridge::cartridge(const std::filesystem::path &p)
+    : mbc_type{mbc0(std::make_shared<cartridge>(*this))} {
+
   rom_data.reserve(8 * 1024 * 1024);
 
   std::ifstream stream(p, std::ios::binary);
   std::copy(std::istreambuf_iterator<char>(stream), {}, rom_data.begin());
-
   rom_data.shrink_to_fit();
+
+  if (auto expected = headerChecksum(); expected != rom_data.at(0x014D)) {
+    std::cerr << "ROM header checksum failed!\n";
+  }
 }
 
-void cartridge::title() noexcept {
+std::string cartridge::title() noexcept {
+  std::string game_title;
   for (auto start = 0x104; start < 0x134; ++start) {
     game_title.push_back(rom_data[start]);
   }
+
+  return game_title;
 }
 
-void cartridge::manufacturer() noexcept {
-  for (auto start = 0x134; start < 0x143; ++start) {
+std::string cartridge::manufacturer() noexcept {
+
+  std::string manufacturer_code;
+  for (auto start = 0x134; start < 0x143; ++start)
     manufacturer_code.push_back(rom_data[start]);
-  }
+
+  return manufacturer_code;
 }
 
 bool cartridge::CGB() const noexcept {
   return rom_data[0x0143] == 0x0080 ? true : false;
 }
 
-void cartridge::newLicenseCode() noexcept {
-  new_license_code.push_back(rom_data[0x0144]);
-  new_license_code.push_back(rom_data[0x0145]);
+std::string cartridge::newLicenseCode() noexcept {
+  switch (rom_data[0x0144] << 8 | rom_data[0x0145]) {
+  case 0x00: return "none";
+  case 0x13: return "electronic arts";
+  case 0x20: return "kss";
+  case 0x25: return "san-x";
+  case 0x30: return "viacom";
+  case 0x33: return "ocean/acclaim";
+  case 0x37: return "taito";
+  case 0x41: return "ubi soft";
+  case 0x46: return "angel";
+  case 0x50: return "absolute";
+  case 0x53: return "american sammy";
+  case 0x56: return "ljn";
+  case 0x59: return "milton bradley";
+  case 0x64: return "lucasarts";
+  case 0x70: return "infogrames";
+  case 0x73: return "sculptured";
+  case 0x79: return "accolade";
+  case 0x86: return "tokuma shoten i*";
+  case 0x92: return "video system";
+  case 0x96: return "yonezawa/s'pal";
+  case 0x01: return "nintendo";
+  case 0x18: return "hudsonsoft";
+  case 0x22: return "pow";
+  case 0x28: return "kemco japan";
+  case 0x31: return "nintendo";
+  case 0x34: return "konami";
+  case 0x38: return "hudson";
+  case 0x42: return "atlus";
+  case 0x47: return "pullet-proof";
+  case 0x51: return "acclaim";
+  case 0x54: return "konami";
+  case 0x57: return "matchbox";
+  case 0x60: return "titus";
+  case 0x67: return "ocean";
+  case 0x71: return "interplay";
+  case 0x75: return "sci";
+  case 0x80: return "misawa";
+  case 0x87: return "tsukuda ori*";
+  case 0x93: return "ocean/acclaim";
+  case 0x97: return "kaneko";
+  case 0x08: return "capcom";
+  case 0x19: return "b-ai";
+  case 0x24: return "pcm complete";
+  case 0x29: return "seta";
+  case 0x32: return "bandia";
+  case 0x35: return "hector";
+  case 0x39: return "banpresto";
+  case 0x44: return "malibu";
+  case 0x49: return "irem";
+  case 0x52: return "activision";
+  case 0x55: return "hi tech entertainment";
+  case 0x58: return "mattel";
+  case 0x61: return "virgin";
+  case 0x69: return "electronic arts";
+  case 0x72: return "broderbund";
+  case 0x78: return "t*hq";
+  case 0x83: return "lozc";
+  case 0x91: return "chun soft";
+  case 0x95: return "varie";
+  case 0x99: return "pack in soft";
+  default: return "";
+  }
 }
 
 bool cartridge::SGB() const noexcept {
@@ -42,56 +191,169 @@ bool cartridge::SGB() const noexcept {
 
 void cartridge::cartType() noexcept {
   switch (rom_data[0x147]) {
-  case 0x00: cartridge_info.type = cartridge_type::ROM_ONLY; break;
+  case cartridge_type::ROM_ONLY:
+    mbc_type = mbc0{std::make_shared<cartridge>(*this)};
+    has_ram = false;
+    has_battery = false;
+    has_timer = false;
 
-  case 0x01: [[fallthrough]];
-  case 0x02: [[fallthrough]];
-  case 0x03: cartridge_info.type = cartridge_type::ROM_MBC1;
+  case cartridge_type::ROM_MBC1:
+    // mbc_type = mbc1{std::make_shared<cartridge>(*this)};
+    // TODO: implement MBC1
+    has_ram = false;
+    // has battery means the game state can be saved.
+    has_battery = false;
+    has_timer = false;
+    break;
 
-  case 0x05: [[fallthrough]];
-  case 0x06: cartridge_info.type = cartridge_type::ROM_MBC2; break;
+  case cartridge_type::ROM_MBC1_RAM:
+    // mbc_type = mbc1{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = false;
+    has_timer = false;
+    break;
 
-  case 0x0B: [[fallthrough]];
-  case 0x0C: [[fallthrough]];
-  case 0x0D: cartridge_info.type = cartridge_type::ROM_MMM01; break;
+  case cartridge_type::ROM_MBC1_RAM_BATTERY:
+    // mbc_type = mbc1{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = true;
+    has_timer = false;
+    break;
 
-  case 0x0F: [[fallthrough]];
-  case 0x10: [[fallthrough]];
-  case 0x11: [[fallthrough]];
-  case 0x12: [[fallthrough]];
-  case 0x13: cartridge_info.type = cartridge_type::ROM_MBC3; break;
+  case cartridge_type::ROM_MBC2:
+    // mbc_type = mbc2{std::make_shared<cartridge>(*this)};
+    has_ram = false;
+    has_battery = false;
+    has_timer = false;
+    break;
 
-  case 0x19: [[fallthrough]];
-  case 0x1A: [[fallthrough]];
-  case 0x1B: [[fallthrough]];
-  case 0x1C: [[fallthrough]];
-  case 0x1D: [[fallthrough]];
-  case 0x1E: cartridge_info.type = cartridge_type::ROM_MBC5; break;
+  case cartridge_type::ROM_MBC2_RAM_BATTERY:
+    // mbc_type = mbc2{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = true;
+    has_timer = false;
+    break;
+
+  case cartridge_type::ROM_MBC3_TIMER_BATTERY:
+    // mbc_type = mbc3{std::make_shared<cartridge>(*this)};
+    has_ram = false;
+    has_battery = true;
+    has_timer = true;
+    break;
+
+  case cartridge_type::ROM_MBC3_TIMER_RAM_BATTERY:
+    // mbc_type = mbc3{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = true;
+    has_timer = true;
+    break;
+
+  case cartridge_type::ROM_MBC3:
+    // mbc_type = mbc3{std::make_shared<cartridge>(*this)};
+    has_ram = false;
+    has_battery = false;
+    has_timer = false;
+    break;
+
+  case cartridge_type::ROM_MBC3_RAM:
+    // mbc_type = mbc3{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = false;
+    has_timer = false;
+    break;
+
+  case cartridge_type::ROM_MBC3_RAM_BATTERY:
+    // mbc_type = mbc3{std::make_shared<cartridge>(*this)};
+    has_ram = true;
+    has_battery = false;
+    has_timer = true;
+    break;
+
+  default: break;
   }
 }
 
 void cartridge::cartRom() noexcept {
   switch (rom_data[0x0148]) {
-  case 0x00: cartridge_info.rom = cartridge_rom::KB_32; break;
-  case 0x01: cartridge_info.rom = cartridge_rom::KB_64; break;
-  case 0x02: cartridge_info.rom = cartridge_rom::KB_128; break;
-  case 0x03: cartridge_info.rom = cartridge_rom::KB_256; break;
-  case 0x04: cartridge_info.rom = cartridge_rom::KB_512; break;
-  case 0x05: cartridge_info.rom = cartridge_rom::KB_1024; break;
-  case 0x06: cartridge_info.rom = cartridge_rom::KB_2048; break;
-  case 0x52: cartridge_info.rom = cartridge_rom::KB_1152; break;
-  case 0x53: cartridge_info.rom = cartridge_rom::KB_1280; break;
-  case 0x54: cartridge_info.rom = cartridge_rom::KB_1526; break;
+  case cartridge_rom::ROM_KB_32:
+    // sizeof(byte) == 8
+    rom_size = 32 * 8;
+    rom_banks = 2;
+    break;
+
+  case cartridge_rom::ROM_KB_64:
+    rom_size = 64 * 8;
+    rom_banks = 4;
+    break;
+
+  case cartridge_rom::ROM_KB_128:
+    rom_size = 128 * 8;
+    rom_banks = 8;
+    break;
+
+  case cartridge_rom::ROM_KB_256:
+    rom_size = 256 * 8;
+    rom_banks = 16;
+    break;
+
+  case cartridge_rom::ROM_KB_512:
+    rom_size = 512 * 8;
+    rom_banks = 32;
+    break;
+
+  case cartridge_rom::ROM_KB_1024:
+    rom_size = 1024 * 8;
+    rom_banks = 64;
+    break;
+
+  case cartridge_rom::ROM_KB_2048:
+    rom_size = 2048 * 8;
+    rom_banks = 128;
+    break;
+
+  case cartridge_rom::ROM_KB_4096:
+    rom_size = 4096 * 8;
+    rom_banks = 256;
+    break;
+
+  case cartridge_rom::ROM_KB_8192:
+    rom_size = 8192 * 8;
+    rom_banks = 512;
+    break;
   }
 }
 
 void cartridge::cartRam() noexcept {
   switch (rom_data[0x0149]) {
-  case 0x00: cartridge_info.ram = cartridge_ram::None; break;
-  case 0x01: cartridge_info.ram = cartridge_ram::KB_2; break;
-  case 0x02: cartridge_info.ram = cartridge_ram::KB_8; break;
-  case 0x03: cartridge_info.ram = cartridge_ram::KB_32; break;
-  case 0x04: cartridge_info.ram = cartridge_ram::KB_128; break;
+  case cartridge_ram::RAM_None:
+    ram_size = 0;
+    ram_banks = 0;
+    break;
+
+  case cartridge_ram::RAM_KB_2:
+    ram_size = 0;
+    ram_banks = 0;
+    break;
+
+  case cartridge_ram::RAM_KB_8:
+    ram_size = 8 * 8;
+    ram_banks = 1;
+    break;
+
+  case cartridge_ram::RAM_KB_32:
+    ram_size = 32 * 8;
+    ram_banks = 4;
+    break;
+
+  case cartridge_ram::RAM_KB_128:
+    ram_size = 128 * 8;
+    ram_banks = 16;
+    break;
+
+  case cartridge_ram::RAM_KB_64:
+    ram_size = 64 * 8;
+    ram_banks = 8;
+    break;
   }
 }
 
@@ -99,11 +361,162 @@ void cartridge::destinationCode() noexcept {
   destination_code = rom_data[0x014A] ? "JP" : "";
 }
 
-void cartridge::oldLicenseCode() noexcept {
+std::string cartridge::oldLicenseCode() noexcept {
   if (rom_data[0x014B] == 0x0033)
-    old_license_code = new_license_code;
+    return newLicenseCode();
   else {
-    old_license_code.push_back(rom_data[0x014B]);
+    // typing/editing all of these took less than 5 minutes with Vim <3
+    // https://raw.githubusercontent.com/gb-archive/salvage/master/txt-files/gbrom.txt
+    switch (rom_data[0x014B]) {
+    case 0x00: return "none";
+    case 0x09: return "hot-b";
+    case 0x0C: return "elite systems";
+    case 0x19: return "itc entertainment";
+    case 0x1F: return "virgin";
+    case 0x28: return "kotobuki systems";
+    case 0x31: return "nintendo";
+    case 0x34: return "konami";
+    case 0x39: return "banpresto";
+    case 0x41: return "ubi soft";
+    case 0x46: return "angel";
+    case 0x4A: return "virgin";
+    case 0x50: return "absolute";
+    case 0x53: return "american sammy";
+    case 0x56: return "ljn";
+    case 0x5A: return "mindscape";
+    case 0x5D: return "tradewest";
+    case 0x67: return "ocean";
+    case 0x6F: return "electro brain";
+    case 0x72: return "broderbund";
+    case 0x78: return "t*hq";
+    case 0x7C: return "microprose";
+    case 0x83: return "lozc";
+    case 0x8C: return "vic tokai";
+    case 0x91: return "chun soft";
+    case 0x95: return "varie";
+    case 0x99: return "arc";
+    case 0x9C: return "imagineer";
+    case 0xA1: return "hori electric";
+    case 0xA6: return "kawada";
+    case 0xAA: return "broderbund";
+    case 0xAF: return "namco";
+    case 0xB2: return "bandai";
+    case 0xB7: return "snk";
+    case 0xBB: return "sunsoft";
+    case 0xC0: return "taito";
+    case 0xC4: return "*tokuma shoten i";
+    case 0xC8: return "koei";
+    case 0xCB: return "vap";
+    case 0xCE: return "*pony canyon or";
+    case 0xD1: return "sofel";
+    case 0xD4: return "ask kodansha";
+    case 0xD9: return "banpresto";
+    case 0xDD: return "ncs";
+    case 0xE0: return "jaleco";
+    case 0xE3: return "varie";
+    case 0xE8: return "asmik";
+    case 0xEB: return "atlus";
+    case 0xF0: return "a wave";
+    case 0x01: return "nintendo";
+    case 0x0A: return "jaleco";
+    case 0x13: return "electronic arts";
+    case 0x1A: return "yanoman";
+    case 0x24: return "pcm complete";
+    case 0x29: return "seta";
+    case 0x32: return "bandai";
+    case 0x35: return "hector";
+    case 0x3C: return "*entertainment i";
+    case 0x42: return "atlus";
+    case 0x47: return "spectrum holoby";
+    case 0x4D: return "malibu";
+    case 0x51: return "acclaim";
+    case 0x54: return "gametek";
+    case 0x57: return "matchbox";
+    case 0x5B: return "romstar";
+    case 0x60: return "titus";
+    case 0x69: return "electronic arts";
+    case 0x70: return "infogrames";
+    case 0x73: return "sculptered soft";
+    case 0x79: return "accolade";
+    case 0x7F: return "kemco";
+    case 0x86: return "*tokuma shoten i";
+    case 0x8E: return "ape";
+    case 0x92: return "video system";
+    case 0x96: return "yonezawa/s'pal";
+    case 0x9A: return "nihon bussan";
+    case 0x9D: return "banpresto";
+    case 0xA2: return "bandai";
+    case 0xA7: return "takara";
+    case 0xAC: return "toei animation";
+    case 0xB0: return "acclaim";
+    case 0xB4: return "enix";
+    case 0xB9: return "pony canyon";
+    case 0xBD: return "sony imagesoft";
+    case 0xC2: return "kemco";
+    case 0xC5: return "data east";
+    case 0xC9: return "ufl";
+    case 0xCC: return "use";
+    case 0xCF: return "angel";
+    case 0xD2: return "quest";
+    case 0xD6: return "naxat soft";
+    case 0xDA: return "tomy";
+    case 0xDE: return "human";
+    case 0xE1: return "towachiki";
+    case 0xE5: return "epoch";
+    case 0xE9: return "natsume";
+    case 0xEC: return "epic/sony records";
+    case 0xeF: return "xtreme entertainment";
+    case 0x08: return "capcom";
+    case 0x0B: return "coconuts";
+    case 0x18: return "hudsonsoft";
+    case 0x1D: return "clary";
+    case 0x25: return "san-x";
+    case 0x30: return "infogrames";
+    case 0x33: return "see above";
+    case 0x38: return "capcom";
+    case 0x3E: return "gremlin";
+    case 0x44: return "malibu";
+    case 0x49: return "irem";
+    case 0x4F: return "u.s. gold";
+    case 0x52: return "activision";
+    case 0x55: return "park place";
+    case 0x59: return "milton bradley";
+    case 0x5C: return "naxat soft";
+    case 0x61: return "virgin";
+    case 0x6E: return "elite systems";
+    case 0x71: return "interplay";
+    case 0x75: return "the sales curve";
+    case 0x7A: return "triffix entertainment";
+    case 0x80: return "misawa entertainment";
+    case 0x8B: return "bullet-proof software";
+    case 0x8F: return "i'max";
+    case 0x93: return "tsuburava";
+    case 0x97: return "kaneko";
+    case 0x9B: return "tecmo";
+    case 0x9F: return "nova";
+    case 0xA4: return "konami";
+    case 0xA9: return "technos japan";
+    case 0xAD: return "toho";
+    case 0xB1: return "ascii or nexoft";
+    case 0xB6: return "hal";
+    case 0xBA: return "*culture brain o";
+    case 0xBF: return "sammy";
+    case 0xC3: return "squaresoft";
+    case 0xC6: return "tonkin house";
+    case 0xCA: return "ultra";
+    case 0xCD: return "meldac";
+    case 0xD0: return "taito";
+    case 0xD3: return "sigma enterprises";
+    case 0xD7: return "copya systems";
+    case 0xDB: return "ljn";
+    case 0xDF: return "altron";
+    case 0xE2: return "uutaka";
+    case 0xE7: return "athena";
+    case 0xEA: return "king records";
+    case 0xEE: return "igs";
+    case 0xFF: return "ljn";
+    default: return "";
+    }
   }
 }
 
--- include/tmbl/cartridge/cartridge.h
+++ include/tmbl/cartridge/cartridge.h
@@ -2,121 +2,53 @@
 #define CARTRIDGE_h
 
 #include "../config.h"
+#include "mbc/mbc0/mbc0.h"
 #include <filesystem>
 #include <string>
+#include <variant>
 #include <vector>
 
 namespace tmbl::cartridge {
 
 class cartridge final {
 public:
-  void load(const std::filesystem::path &p);
+  explicit cartridge(const std::filesystem::path &p);
 
-  void title() noexcept;
-  void manufacturer() noexcept;
+  std::string title() noexcept;
+  std::string manufacturer() noexcept;
 
   bool CGB() const noexcept;
-  void newLicenseCode() noexcept;
+  std::string newLicenseCode() noexcept;
   bool SGB() const noexcept;
 
   void cartType() noexcept;
   void cartRom() noexcept;
   void cartRam() noexcept;
   void destinationCode() noexcept;
-  void oldLicenseCode() noexcept;
+  std::string oldLicenseCode() noexcept;
   void romVersion() noexcept;
   byte headerChecksum() const noexcept;
 
-  enum class cartridge_type {
-    ROM_ONLY = 0x00,
-
-    ROM_MBC1 = 0x01,
-    ROM_MBC1_RAM = 0x02,
-    ROM_MBC1_RAM_BATTERY = 0x03,
-
-    ROM_MBC2 = 0x05,
-    ROM_MBC2_RAM_BATTERY = 0x06,
-
-    ROM_RAM = 0x08,
-    ROM_RAM_BATTERY = 0x09,
-
-    ROM_MMM01 = 0x0B,
-    ROM_MMM01_SRAM = 0x0C,
-    ROM_MMM01_SRAM_BATTERY = 0x0D,
-
-    ROM_MBC3_TIMER_BATTERY = 0x0F,
-    ROM_MBC3_TIMER_RAM_BATTERY = 0x10,
-    ROM_MBC3 = 0x11,
-    ROM_MBC3_RAM = 0x12,
-    ROM_MBC3_RAM_BATTERY = 0x13,
-
-    ROM_MBC5 = 0x19,
-    ROM_MBC5_RAM = 0x1A,
-    ROM_MBC5_RAM_BATTERY = 0x1B,
-
-    ROM_MBC5_RUMBLE = 0x1C,
-    ROM_MBC5_RUMBLE_SRAM = 0x1D,
-    ROM_MBC5_RUMBLE_SRAM_BATTERY = 0x1E,
-
-    ROM_MBC6_RAM_BATTERY = 0x20,
-    ROM_MBC7_RAM_BATTERY_ACCELEROMETER = 0x22,
-
-    POCKET_CAMERA = 0xFC,
-    BANDAI_TAMA5 = 0xFD,
-
-    Hudson_HuC_3 = 0xFE,
-    Hudson_HuC_1 = 0xFF
-  };
-
-  enum class cartridge_rom {
-    KB_32 = 0x00,
-    KB_64 = 0x01,
-    KB_128 = 0x02,
-    KB_256 = 0x03,
-    KB_512 = 0x04,
-    KB_1024 = 0x05,
-    KB_2048 = 0x06,
-    KB_4096 = 0x07,
-    KB_8192 = 0x08,
-    KB_1152 = 0x52,
-    KB_1280 = 0x53,
-    KB_1526 = 0x54
-  };
-
-  enum class cartridge_ram {
-    None = 0x00,
-    KB_2 = 0x01,
-    KB_8 = 0x02,
-    KB_32 = 0x03,
-    KB_128 = 0x04,
-    KB_64 = 0x05
-  };
-
-  enum class gameboy_type {
-    DMG, // Dot matrix Game Boy, 1989
-    MGB, // Game Boy Pocket, 1996
-    MGL, // Game Boy Light, 1998
-    CGB, // Game Boy Color, 1998
-    SGB, // Super Game Boy, 1994,
-    SGB2 // Super Game Boy 2, 1998
-  };
-
 private:
-  struct {
-    cartridge_type type;
-    cartridge_rom rom;
-    cartridge_ram ram;
-    bool has_battery = false;
-  } cartridge_info;
-
   std::vector<byte> rom_data;
+  std::vector<byte> ram_data;
 
-  std::string game_title;
-  std::string manufacturer_code;
-  std::string new_license_code;
+  bool has_ram = false;
+  bool has_battery = false;
+  bool has_timer = false;
+
+  bool ram_disabled = false;
+
+  int rom_size = 0;
+  int ram_size = 0;
+
+  int rom_banks = 0;
+  int ram_banks = 0;
+
   std::string destination_code;
-  std::string old_license_code;
   byte rom_version;
+
+  std::variant<mbc0 /*, mbc1, mbc2, mbc3, mbc5*/> mbc_type;
 };
 
 } // namespace tmbl::cartridge
