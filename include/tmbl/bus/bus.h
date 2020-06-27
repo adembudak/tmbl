d53949ec78b42de46d06e256715a4dba19427506
Adem Budak <admbudk@gmail.com>
2020-06-27 13:59:10 +0300

    Pinpoint memory areas.

--- include/tmbl/bus/bus.h
+++ include/tmbl/bus/bus.h
@@ -26,8 +26,11 @@ public:
   void write(const cpu::reg16 rr, const byte b);
   void write(const cpu::reg16 rr1, const cpu::reg16 rr2);
 
-  byte *const rom_begin = std::data(m_data) + 0x0000U;
-  byte *const rom_end = std::data(m_data) + 0x07FFFU;
+  byte *const rom0_begin = std::data(m_data) + 0x0000U;
+  byte *const rom0_end = std::data(m_data) + 0x3FFFU;
+
+  byte *const romN_begin = std::data(m_data) + 0x4000U;
+  byte *const romN_end = std::data(m_data) + 0x07FFFU;
 
   byte *const vram_begin = std::data(m_data) + 0x8000U;
   byte *const vram_end = std::data(m_data) + 0x9FFFU;
@@ -35,14 +38,20 @@ public:
   byte *const sram_begin = std::data(m_data) + 0xA000U;
   byte *const sram_end = std::data(m_data) + 0xBFFFU;
 
-  byte *const wram_begin = std::data(m_data) + 0xC000U;
-  byte *const wram_end = std::data(m_data) + 0xDFFFU;
+  byte *const wram0_begin = std::data(m_data) + 0xC000U;
+  byte *const wram0_end = std::data(m_data) + 0xCFFFU;
+
+  byte *const wramN_begin = std::data(m_data) + 0xD000U;
+  byte *const wramN_end = std::data(m_data) + 0xDFFFU;
 
   byte *const wram_echo_begin = std::data(m_data) + 0xE000U;
   byte *const wram_echo_end = std::data(m_data) + 0xFDFFU;
 
   byte *const oam_begin = std::data(m_data) + 0xFE00U;
   byte *const oam_end = std::data(m_data) + 0xFE9FU;
+
+  byte *const unused_begin = std::data(m_data) + 0xFEA0U;
+  byte *const unused_end = std::data(m_data) + 0xFEFFU;
 
   byte *const io_begin = std::data(m_data) + 0xFF00U;
   byte *const io_end = std::data(m_data) + 0xFF7FU;
