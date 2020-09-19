6ad64cb851007c5294b431dffa9e6ff37f9f5cdf
Adem Budak <admbudk@gmail.com>
2020-09-19 16:56:33 +0300

    Added: A ctor overload to r8.
    Added: nibble values accessors to r8.
    
    Added: nonmember operator+ for two r8 types.
    
    Added: nonmember operator+ for r8 and uint8 types.
    
    Added: nonmber operator== for r8 and uint8 types.

--- include/tmbl/cpu/internals/r8.h
+++ include/tmbl/cpu/internals/r8.h
@@ -1,19 +1,28 @@
 #include "../../config.h"
 
 namespace tmbl {
-
 class n8;
 
 class r8 final {
 public:
+  r8() = default;
+  explicit r8(const uint8 val);
+
   //  r8 &operator=(const r8 n); //Spe. Mem. Implicit,  ADC A, r8
   r8 &operator=(const byte b); // ADC A, [HL]
   r8 &operator=(const n8 n);   // ADC A, n8
+
+  uint8 loNibble() const noexcept;
+  uint8 hiNibble() const noexcept;
 
   uint8 value() const noexcept;
 
 private:
   uint8 m_data = 0;
 };
+
+r8 operator+(const r8 l, const r8 r);
+uint8 operator+(const r8 r, const uint8 u);
+bool operator==(const r8 r, uint8 u);
 
 }
--- src/cpu/internals/r8.cpp
+++ src/cpu/internals/r8.cpp
@@ -1,7 +1,25 @@
 #include "tmbl/cpu/internals/r8.h"
+#include "tmbl/cpu/internals/n8.h"
+
 namespace tmbl {
+
+r8 &r8::operator=(const byte b) {
+  m_data = b;
+  return *this;
+}
+
+r8 &r8::operator=(const n8 n) {
+  m_data = n.value();
+  return *this;
+}
+uint8 r8::loNibble() const noexcept { return m_data & 0b0000'1111; }
+
+uint8 r8::hiNibble() const noexcept { return (m_data & 0b1111'0000) >> 4; }
 
 uint8 r8::value() const noexcept { return m_data; }
 
-}
+r8 operator+(const r8 l, const r8 r) { return r8(l.value() + r.value()); }
+uint8 operator+(const r8 r, const uint8 u) { return r.value() + u; }
+bool operator==(const r8 r, uint8 u) { return r.value() == u; }
 
+}
