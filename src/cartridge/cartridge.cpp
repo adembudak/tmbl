ee04d0a9fd65fc5e03e8ae6a06026a088dd8fa0d
Adem Budak <admbudk@gmail.com>
2020-10-15 23:35:25 +0000

    Added: error check on dumped rom.

--- src/cartridge/cartridge.cpp
+++ src/cartridge/cartridge.cpp
@@ -11,19 +11,25 @@
 #include <variant>
 #include <vector>
 
+#include <iostream>
+
 namespace tmbl {
 
 inline std::vector<byte> dumpROM(const std::filesystem::path &p) {
   std::fstream f(p, std::ios::in | std::ios::binary);
+  if (!f.good())
+    std::cerr << "Something wrong with the rom.\n";
   return {std::istreambuf_iterator(f), {}};
 }
 
 cartridge::cartridge(const std::filesystem::path &p) : dumpedGamePak{dumpROM(p)} {
 
+  // decide game title
   const std::size_t title = 0x0134;
   const std::size_t title_end = 0x013E + 1; // +1 for the use of iterator overload of std::string.
   m_title = std::string(&dumpedGamePak[title], &dumpedGamePak[title_end]);
 
+  // whether cart suuport for color gameboy
   const std::size_t CGB_support_code = 0x0143;
   if (dumpedGamePak[CGB_support_code] == 0x0080 || dumpedGamePak[CGB_support_code] == 0x00C0) {
     m_cgb_support = true;
@@ -31,6 +37,7 @@ cartridge::cartridge(const std::filesystem::path &p) : dumpedGamePak{dumpROM(p)}
     m_cgb_support = false;
   }
 
+  // decide to mbc type
   const std::size_t pak_type = 0x0147;
   const std::size_t pak_rom_size = 0x0148;
   const std::size_t pak_xram_size = 0x0149;
