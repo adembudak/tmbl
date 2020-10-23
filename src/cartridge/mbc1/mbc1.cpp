7e634478b65d258a0b1cd119f8ffca5bace8c6be
Adem Budak <admbudk@gmail.com>
2020-10-23 04:35:49 +0300

    Remove redundant checks for 0x20, 0x40, 0x60 values.

--- src/cartridge/mbc1/mbc1.cpp
+++ src/cartridge/mbc1/mbc1.cpp
@@ -7,8 +7,6 @@
 
 namespace tmbl {
 
-
-
 mbc1::mbc1(std::vector<byte> &rom, std::size_t xram_size) : m_rom(rom) {
   if (xram_size != 0) {
     m_xram.resize(xram_size);
@@ -18,26 +16,16 @@ mbc1::mbc1(std::vector<byte> &rom, std::size_t xram_size) : m_rom(rom) {
 void mbc1::write(const std::size_t index, const byte val) {
 
   if (index >= 0x0000 && index <= 0x1FFF) { // ram chip select
-    if ((val & 0b0000'1111) == 0x0A) {
-      ram_access_enabled = true;
-    } else {
-      ram_access_enabled = false;
-    }
+    ram_access_enabled = ((val & 0b0000'1111) == 0x0A);
   }
 
   else if (index >= 0x2000 && index <= 0x3FFF) { // lower rom bank select
-    if (uint8 ret = val & 0x10; ret == 0 || ret == 20 || ret == 40 || ret == 60) {
+    if ((val & 0x1F) == 0) {
       lower_rom_bank = val + 1;
       return;
     }
 
-    if (val >= 0x01 && val <= 0x1F) {
-      lower_rom_bank = val & lower_rom_mask;
-      if (lower_rom_bank == 0 || lower_rom_bank == 20 || lower_rom_bank == 40 ||
-          lower_rom_bank == 60) {
-        ++lower_rom_bank;
-      }
-    }
+    lower_rom_bank = val & lower_rom_mask;
   }
 
   else if (index >= 0x4000 && index <= 0x5FFF) { // decide upper rom bank or ram bank number
@@ -57,12 +45,10 @@ void mbc1::write(const std::size_t index, const byte val) {
 
   else if (index >= 0x6000 && index <= 0x7FFF) {
     if (m_rom.size() > 512_KB || m_xram.size() > 8_KB) {
-      if ((val & 0b0000'0001) == 0) {
-        banking_mode = mode::rom_banking;
-      } else {
-        banking_mode = mode::ram_banking;
-      }
+      banking_mode = (val & 0b0000'0001) ? mode::ram_banking : mode::rom_banking;
     }
   }
 }
+
 }
+
