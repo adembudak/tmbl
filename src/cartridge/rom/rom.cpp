8b4e50263b27e8f2ac253bff0f54cc91d6314ff0
Adem Budak <admbudk@gmail.com>
2020-10-25 07:08:36 +0300

    Generalize ROM pak type to use ROM+RAM kind of paks.

--- src/cartridge/rom/rom.cpp
+++ src/cartridge/rom/rom.cpp
@@ -4,10 +4,19 @@
 
 namespace tmbl {
 
-rom::rom(std::vector<char> &rom_) : m_rom(std::move(rom_)) {}
+rom::rom(std::vector<char> &&rom_, int xram_size) : m_rom(std::move(rom_)) {
+  if (xram_size > 0) {
+    m_xram.resize(xram_size);
+  }
+}
 
 byte rom::read_rom(const std::size_t index) { return m_rom.at(index); }
-byte rom::read_xram(const std::size_t index) { return m_xram.at(index); }
-byte rom::write_xram(const std::size_t index, const byte val) { return m_xram.at(index) = val; }
+byte rom::read_xram(const std::size_t index) { return m_xram.size() ? m_xram.at(index) : 0xFF; }
+
+void rom::write_xram(const std::size_t index, const byte val) {
+  if (m_xram.size() > 0) {
+    m_xram.at(index) = val;
+  }
+}
 
 }
