556890c49b97e2e83f1dd5ce0ad669b8b1d532b8
Adem Budak <adembudak@outlook.com>
2020-10-27 00:44:19 +0300

    Don't try to catch exceptions.

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,13 +6,9 @@
 int main(int argc, char *argv[]) {
 
   if (argc > 1) {
-    try {
       gameboy myboy;
       myboy.plugCart(argv[1]);
       myboy.run();
-    } catch (...) {
-      std::cerr << "ROM is file: " << argv[1] << " is not exist.\n";
-    }
   } else {
     std::cerr << "Pass a ROM file.\n";
   }
