fe90d92565ec52db9275a7a46235dbd97d6ebbe9
Adem Budak <admbudk@gmail.com>
2020-11-14 21:05:10 +0300

    Added: lcdc bit 0 color gameboy condition.

--- include/tmbl/ppu/internals/lcdc.h
+++ include/tmbl/ppu/internals/lcdc.h
@@ -8,21 +8,29 @@ namespace tmbl {
 
 class lcdc {
 public:
-  explicit lcdc(byte &val_);
+  explicit lcdc(byte &val_, bool cgb_support);
 
   // https://archive.org/details/GameBoyProgManVer1.1/page/n56/mode/1up
+  // terminology:
+  // Gameboy Programming Manual | Gameboy PanDocs
+  // -------------------------------------
+  // Object(OBJ)                | Sprite
+  // Character(CHR)             | Window
+  // Background(BG)             | <-
+  // Code area                  | tile map
 
-  cflag bgDisplayStatus() const noexcept;
-  cflag objDisplayStatus() const noexcept;
-  std::pair<uint8, uint8> objSize() const noexcept;
-  std::pair<uint16, uint16> bgCodeArea() const noexcept;
-  std::pair<uint16, uint16> bgChrArea() const noexcept;
+  cflag lcdControllerStatus() const noexcept;                // bit 7
+  std::pair<uint16, uint16> windowCodeArea() const noexcept; // map select
   cflag windowStatus() const noexcept;
-  std::pair<uint16, uint16> windowCodeArea() const noexcept;
-  cflag lcdControllerStatus() const noexcept;
+  std::pair<uint16, uint16> bgChrArea() const noexcept;
+  std::pair<uint16, uint16> bgCodeArea() const noexcept; // map select
+  std::pair<uint8, uint8> objSize() const noexcept;
+  cflag objDisplayStatus() const noexcept;
+  cflag bgDisplayStatus() const noexcept; // bit 0
 
 private:
   byte &m_value;
+  bool cgb_support = false;
 };
 
 }
--- src/ppu/internals/lcdc.cpp
+++ src/ppu/internals/lcdc.cpp
@@ -4,31 +4,31 @@ namespace tmbl {
 
 class registers;
 
-lcdc::lcdc(byte &val_) : m_value(val_) {}
+lcdc::lcdc(byte &val_, bool cgb_support) : m_value(val_), cgb_support(cgb_support) {}
 
-cflag lcdc::bgDisplayStatus() const noexcept {
-  return /*m_pCart->CGB() ? on :*/ m_value | 0b0000'0001;
+cflag lcdc::lcdControllerStatus() const noexcept { return m_value & 0b1000'0000 ? on : off; }
+
+std::pair<uint16, uint16> lcdc::windowCodeArea() const noexcept {
+  return m_value & 0b0100'0000 ? std::make_pair(0x9C00, 0x9FFF) : std::make_pair(0x9800, 0x9BFF);
 }
 
-cflag lcdc::objDisplayStatus() const noexcept { return m_value & 0b0000'0010 ? on : off; }
+cflag lcdc::windowStatus() const noexcept { return m_value & 0b0010'0000 ? on : off; }
 
-std::pair<uint8, uint8> lcdc::objSize() const noexcept {
-  return m_value & 0b0000'0100 ? std::make_pair(8, 16) : std::make_pair(8, 8);
+std::pair<uint16, uint16> lcdc::bgChrArea() const noexcept {
+  return m_value & 0b0001'0000 ? std::make_pair(0x8000, 0x8FFF) : std::make_pair(0x8800, 0x97FF);
 }
 
 std::pair<uint16, uint16> lcdc::bgCodeArea() const noexcept {
   return m_value & 0b0000'1000 ? std::make_pair(0x9C00, 0x9FFF) : std::make_pair(0x9800, 0x9BFF);
 }
 
-std::pair<uint16, uint16> lcdc::bgChrArea() const noexcept {
-  return m_value & 0b0001'0000 ? std::make_pair(0x8000, 0x8FFF) : std::make_pair(0x8800, 0x97FF);
+std::pair<uint8, uint8> lcdc::objSize() const noexcept {
+  return m_value & 0b0000'0100 ? std::make_pair(8, 16) : std::make_pair(8, 8);
 }
 
-cflag lcdc::windowStatus() const noexcept { return m_value & 0b0010'0000 ? on : off; }
+cflag lcdc::objDisplayStatus() const noexcept { return m_value & 0b0000'0010 ? on : off; }
 
-std::pair<uint16, uint16> lcdc::windowCodeArea() const noexcept {
-  return m_value & 0b0100'0000 ? std::make_pair(0x9C00, 0x9FFF) : std::make_pair(0x9800, 0x9BFF);
+cflag lcdc::bgDisplayStatus() const noexcept { return cgb_support ? on : m_value & 0b0000'0001; }
+
 }
 
-cflag lcdc::lcdControllerStatus() const noexcept { return m_value & 0b1000'0000 ? on : off; }
-}
