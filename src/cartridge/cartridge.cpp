42c814e1c6788f032876235abd4f3a3a97575fb2
Adem Budak <admbudk@gmail.com>
2020-11-16 01:43:47 +0300

    Fix typo: PACK->PAK

--- src/cartridge/cartridge.cpp
+++ src/cartridge/cartridge.cpp
@@ -15,7 +15,7 @@
 #include <iostream>
 #include <cassert>
 
-#if defined PRINT_PACK_INFO
+#if defined PRINT_PAK_INFO
 #include <algorithm> // for std::copy_n
 #include "tmbl/cartridge/metadata/pakInfo.h"
 #endif
@@ -33,7 +33,7 @@ bool cartridge::init(const std::filesystem::path p) {
 
     auto dumpedGamePak = std::vector<byte>(std::istream_iterator<byte>(f), {});
 
-#if defined PRINT_PACK_INFO
+#if defined PRINT_PAK_INFO
     std::vector<byte> cartridge_header;
     std::copy_n(begin(dumpedGamePak), 0x014F + 1, begin(cartridge_header));
     std::cerr << metadata::pakInfo(std::move(cartridge_header));
