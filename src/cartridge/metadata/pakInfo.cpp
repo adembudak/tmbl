c62db85012289c43eacc4b9fdb0fac89325a2783
Adem Budak <adembudak@outlook.com>
2020-11-15 23:29:24 +0300

    Fix some formatting mistakes.

--- src/cartridge/metadata/pakInfo.cpp
+++ src/cartridge/metadata/pakInfo.cpp
@@ -18,8 +18,6 @@ pakInfo::pakInfo(std::vector<byte> &&vec) : cartridge_header(std::move(vec)) {
 
   new_licensee_code = ((cartridge_header[0x0144] & 0x0F) << 8) | (cartridge_header[0x0145] & 0x0F);
 
-  // set sgb_flag
-
   // clang-format off
   sgb_flag =                cartridge_header[0x0146];
   cartridge_type =          cartridge_header[0x0147];
@@ -34,28 +32,26 @@ pakInfo::pakInfo(std::vector<byte> &&vec) : cartridge_header(std::move(vec)) {
 std::ostream &operator<<(std::ostream &os, const pakInfo &pak) {
 
   os << std::string(50, '~') << '\n';
-
-  os << "nintendo logo check: " << std::boolalpha << pak.nintendo_logo_check << '\n';
+  os << "Nintendo logo check: " << std::boolalpha << pak.nintendo_logo_check << '\n';
   os << "title: " << pak.title << '\n';
   os << "manufacturer code: " << pak.manufacturer_code << '\n'; // for "newer" cartridges
-  os << "CGB support: [00h: no, 80h: compatible, C0h: exclusive] " << std::hex << pak.cgb_flag
+  os << "CGB support [00h: no, 80h: compatible, C0h: exclusive]: " << std::hex << pak.cgb_flag
      << '\n';
 
   os << "new licensee code: " << std::hex << pak.new_licensee_code << ':'
      << new_licensee[pak.new_licensee_code] << '\n';
-  os << "SGB support: [00h: incompatible, 03h: uses sgb features] " << std::hex << pak.sgb_flag
+  os << "SGB support [00h: incompatible, 03h: uses sgb features]: " << std::hex << pak.sgb_flag
      << '\n';
-
-  // print out sgb support
-
   os << "cartridge type: " << cartridge_types[pak.cartridge_type] << '\n';
   os << "rom size: " << rom_sizes[pak.rom_size] << '\n';
-  os << "ram size: " << rom_sizes[pak.ram_size] << '\n';
-  os << "destination code [0x00: JP, 0x01: all other]: " << pak.destination_code << '\n';
+  os << "ram size: " << ram_sizes[pak.ram_size] << '\n';
+  os << "destination code [0x00: JP, 0x01: all other]: " << std::hex << pak.destination_code
+     << '\n';
   os << "old licensee code: " << std::hex << pak.old_licensee_code << ':'
      << old_licensee[pak.old_licensee_code] << '\n';
 
   os << "mask rom version number: " << pak.mask_rom_version_number << '\n';
+  os << std::string(50, '~') << '\n';
 
   return os;
 }
