ad004542db2be6e45fcea0fab73f763c2e15c0ec
Adem Budak <adembudak@outlook.com>
2020-11-17 23:32:46 +0300

    Some retouches on stat register.

--- include/tmbl/ppu/internals/stat.h
+++ include/tmbl/ppu/internals/stat.h
@@ -5,12 +5,13 @@
 #include "../../config.h"
 
 namespace tmbl {
+// https://gbdev.io/pandocs/#ff41-stat-lcdc-status-r-w
+// https://archive.org/details/GameBoyProgManVer1.1/page/n57/mode/1up
 
 class stat {
 public:
   explicit stat(byte &val_);
 
-  // https://archive.org/details/GameBoyProgManVer1.1/page/n57/mode/1up
   enum class mode : uint8 {
     HORIZONTAL_BLANKING = 0b00,
     VERTICAL_BLANKING = 0b01,
@@ -18,16 +19,16 @@ public:
     TRANSFERING_DATA_TO_LCD = 0b11,
   };
 
-  mode mode_flag() const noexcept;
-  void mode_flag(const mode m) noexcept;
+  mode mode_flag() const noexcept;       // bit0 bit1
+  void mode_flag(const mode m) noexcept; // bit0 bit1
 
-  cflag match_flag() const noexcept;
-  void match_flag(cflag val) noexcept;
+  cflag match_flag() const noexcept;   // bit2
+  void match_flag(cflag val) noexcept; // bit2
 
-  cflag matchHblank() { return m_value & 0b0000'1000; }
-  cflag matchVblank() { return m_value & 0b0001'0000; }
-  cflag matchSearchOAM() { return m_value & 0b0010'0000; }
-  cflag matchCoincidence() { return m_value & 0b0100'0000; }
+  cflag matchHblank() const noexcept;      // bit3
+  cflag matchVblank() const noexcept;      // bit4
+  cflag matchSearchOAM() const noexcept;   // bit5
+  cflag matchCoincidence() const noexcept; // bit6
 
 private:
   byte &m_value;
@@ -36,4 +37,3 @@ private:
 }
 
 #endif
-
--- src/ppu/internals/stat.cpp
+++ src/ppu/internals/stat.cpp
@@ -8,38 +8,47 @@ class registers;
 stat::stat(byte &val_) : m_value(val_) {}
 
 stat::mode stat::mode_flag() const noexcept {
-  // clang-format off
+
   switch (m_value & 0b0000'0011) {
+      // clang-format off
     case 0: return mode::HORIZONTAL_BLANKING;
     case 1: return mode::VERTICAL_BLANKING;
     case 2: return mode::SEARCHING_OAM;
     case 3: return mode::TRANSFERING_DATA_TO_LCD;
     default: break;
-  // clang-format on 
+      // clang-format on
   }
 }
 
 void stat::mode_flag(const mode m) noexcept {
   switch (m) {
     case mode::HORIZONTAL_BLANKING:
-      m_value |= 0b00;
+      m_value = (m_value & 0b1111'1100); // 0b00
       break;
+
     case mode::VERTICAL_BLANKING:
-      m_value |= 0b01;
+      m_value = (m_value & 0b1111'1100) | 1; // 0b01
       break;
+
     case mode::SEARCHING_OAM:
-      m_value |= 0b10;
+      m_value = (m_value & 0b1111'1100) | 2; // 0b10
       break;
+
     case mode::TRANSFERING_DATA_TO_LCD:
-      m_value |= 0b11;
+      m_value = (m_value & 0b1111'1100) | 3; // 0b11
       break;
+
     default:
       break;
   }
 }
 
 cflag stat::match_flag() const noexcept { return false; /*return LY == LYC;*/ }
-
 void stat::match_flag(cflag val) noexcept { val ? m_value |= 0b0000'0100 : m_value &= 0b1111'1011; }
 
-};
+cflag stat::matchHblank() const noexcept { return m_value & 0b0000'1000; }
+cflag stat::matchVblank() const noexcept { return m_value & 0b0001'0000; }
+cflag stat::matchSearchOAM() const noexcept { return m_value & 0b0010'0000; }
+cflag stat::matchCoincidence() const noexcept { return m_value & 0b0100'0000; }
+
+}
