395eb4cd02ede101be5e0e568624bb34b58e06ab
Adem Budak <admbudk@gmail.com>
2021-01-19 01:27:23 +0300

    Merge timer and clock logics.

--- include/tmbl/clock/clock.h
+++ include/tmbl/clock/clock.h
@@ -2,6 +2,7 @@
 #define CLOCK_H
 
 #include "../config.h"
+#include "../io/registers.h"
 
 #include <chrono>
 #include <ratio>
@@ -11,11 +12,11 @@
   4.194304 MHz in terms of clock cycle and at 1.048576 MHz in machine cycle
 
   In double speed mode, it's
-  8.388608 MHz in terms of clock cycle and at 2.097153 MHz in machine cycle
+  8.388608 MHz clock cycle and 2.097153 MHz in machine cycle
 
-  This emulator used machine cycle:
+  This emulator used machine cycles:
   f = 1.048576 MHz = 1048576 Hz, T = 0.000953657 ms ~ 954 us
-  f = 2.097153 MHz = 2097153 Hz, T = 0.000476837 ms - 477 us
+  f = 2.097153 MHz = 2097153 Hz, T = 0.000476837 ms ~ 477 us
  */
 namespace tmbl {
 
@@ -25,17 +26,53 @@ public:
   using normal_speed = std::ratio<954>;
   using double_speed = std::ratio<477>;
 
-  using normal_speed_duration =
+  using normal_speed_cycle_period =
       std::chrono::duration<int, std::ratio_multiply<normal_speed, std::micro>>;
-
-  using double_speed_duration =
+  using double_speed_cycle_period =
       std::chrono::duration<int, std::ratio_multiply<double_speed, std::micro>>;
 
+  clock(const clock &other) noexcept = delete;
+  clock &operator=(const clock &rhs) noexcept = delete;
+
+  clock(clock &&other) noexcept = delete;
+  clock &operator=(clock &&rhs) noexcept = delete;
+
+  // singleton
+  static clock &instance(registers &r) {
+    static clock c(r);
+    return c;
+  }
+
   void cycle(const uint8 n) noexcept;
-  void enableDoubleSpeedMode(const bool val);
+  void enableDoubleSpeedMode(const bool val) noexcept;
+  void resetDIV() noexcept;
+
+  /*
+   // TODO: how to implement TIMA - TMA logic?
+    if (TIMA > std::numeric_limits<byte>::max()) {
+      TIMA = TMA;
+      timer_interrupt_irc = set;
+  */
 
 private:
+  explicit clock(registers &reg_);
+  ~clock() = default;
+
+private:
+  [[nodiscard]] bool timerOn() const noexcept;
+  uint16 timerController() const noexcept;
+
+  registers &m_reg;
   bool m_double_speed_mode = false;
+  uint16 m_base_freq = 4194304; /* in Hertz */
+
+  uint64 global_counter = 0;
+  uint8 div_counter = 0;
+
+  byte &DIV;
+  byte &TIMA;
+  byte &TMA;
+  byte &TAC;
 };
 
 }
--- src/clock/clock.cpp
+++ src/clock/clock.cpp
@@ -5,14 +5,47 @@
 
 namespace tmbl {
 
+// clang-format off
+clock::clock(registers &reg_)
+    : m_reg(reg_), 
+    DIV(m_reg.getAt(0xFF04)), 
+    TIMA(m_reg.getAt(0xFF05)), 
+    TMA(m_reg.getAt(0xFF06)),
+    TAC(m_reg.getAt(0xFF07)) {}
+// clang-format on
+
 void clock::cycle(const uint8 n) noexcept {
+  using namespace std::this_thread;
+
+  global_counter += n;
+  div_counter += n;
+
+  if (div_counter > 64) {
+    div_counter = 0;
+    DIV++;
+  }
+
   if (m_double_speed_mode) {
-    std::this_thread::sleep_for(double_speed_duration{n});
+    sleep_for(double_speed_cycle_period{n});
   } else {
-    std::this_thread::sleep_for(normal_speed_duration{n});
+    sleep_for(normal_speed_cycle_period{n});
   }
 }
 
-void clock::enableDoubleSpeedMode(const bool val) { m_double_speed_mode = val; }
+void clock::enableDoubleSpeedMode(const bool val) noexcept { m_double_speed_mode = val; }
+bool clock::timerOn() const noexcept { return TAC & 0b0000'0100; }
+void clock::resetDIV() noexcept { DIV = 0; }
 
+uint16 clock::timerController() const noexcept {
+  const uint16 freq = m_double_speed_mode ? 2 * m_base_freq : m_base_freq;
+
+  // clang-format off
+  switch (TAC & 0b0000'0011) {
+    case 0b00: return freq / 1024; // 4096 Hz
+    case 0b01: return freq / 16;   // 252144 Hz
+    case 0b10: return freq / 64;   // 65536 Hz
+    case 0b11: return freq / 256;  // 16384 Hz
+  }
 }
+}
+
