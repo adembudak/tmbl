aab2a8f87b1fdd47a4022257518591589f34e31f
Adem Budak <admbudk@gmail.com>
2021-05-05 02:27:02 +0300

    Initial mbc2

---
+++ include/tmbl/cartridge/type/mbc2.h
@@ -0,0 +1,25 @@
+#ifndef MBC2
+#define MBC2
+
+#include "tmbl/config.h"
+#include <vector>
+
+namespace tmbl {
+class mbc2 final {
+public:
+  explicit mbc2(std::vector<byte> &&rom, const std::size_t xram_size = 0);
+
+  byte read(const std::size_t index) noexcept;
+  void write(const std::size_t index, const byte val) noexcept;
+
+private:
+  std::vector<byte> m_xram{};
+
+  flag xram_access_enabled = false; // ramg, ram gate register
+  byte romb = 1;                    // rom bank register, 1 by default
+
+  std::vector<byte> m_rom{};
+};
+
+}
+#endif
---
+++ src/cartridge/type/mbc2.cpp
@@ -0,0 +1,59 @@
+#include <tmbl/cartridge/type/mbc2.h>
+
+#include <cstdlib>
+
+namespace tmbl {
+
+mbc2::mbc2(std::vector<byte> &&rom, const std::size_t xram_size) {
+  m_rom = std::move(rom);
+  m_xram.resize(xram_size);
+}
+
+void mbc2::write(const std::size_t index, const byte val) noexcept {
+  if (index >= 0x0000 && index <= 0x3FFF) { // ramg and romb registers
+    if (val & 0b1000'0000) {
+      romb = ((val & 0b0000'1111) == 0) ? 1 : (val & 0b0000'1111);
+    } else {
+      xram_access_enabled = (val & 0b0000'1111) == 0b1010;
+    }
+  }
+
+  else if (index >= 0xA000 && index <= 0xBFFF) { // RAM (read/'write')
+    if (xram_access_enabled) {
+      const byte data = val & 0b0000'1111; // mbc2 ram has only 4 bit of datum
+      m_xram.at(index % 512) = data;
+    } else {
+      (void)val;
+    }
+  }
+
+  else {
+  }
+}
+
+byte mbc2::read(const std::size_t index) noexcept { // ROM Bank 0 (Read only)
+  if (index >= 0x0000 && index <= 0x3FFF) {         // bank 0
+    return m_rom.at(index);
+  }
+
+  else if (index >= 0x4000 && index <= 0x7FFF) { // ROM Bank 1-15
+    std::size_t effective_index = (romb << 14) | (index & 0b11'1111'1111'1111);
+
+    return m_rom.at(index);
+  }
+
+  else if (index >= 0xA000 && index <= 0xBFFF) { // RAM ('read'/write)
+    if (xram_access_enabled) {
+      const byte data = m_xram.at(index % 512) & 0b0000'1111;
+      return data;
+    } else {
+      return std::rand();
+    }
+  }
+
+  else {
+    // things
+  }
+}
+
+}
