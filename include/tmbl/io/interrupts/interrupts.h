d7d31525201ed71802744bab5960de8e4e72a4fe
Adem Budak <admbudk@gmail.com>
2021-05-14 17:57:54 +0300

    Rewrite interrupt class

--- include/tmbl/io/interrupts/interrupts.h
+++ include/tmbl/io/interrupts/interrupts.h
@@ -15,20 +15,20 @@ public:
   byte read(const std::size_t index) const noexcept;
   void write(const std::size_t index, const byte val) noexcept;
 
-  uint8 IF() const noexcept; // 0xFF0F, interrupt flag
-  uint8 IE() const noexcept; // 0xFFFF, interrupt enabled
+  uint8 IF() const noexcept; // 0xFF0F, interrupt request flag
+  uint8 IE() const noexcept; // 0xFFFF, interrupt enabled flag
 
-  bool VBlank_IRQ = false;                     //  vertical blanking intr requested
-  bool LCDC_Status_IRQ = false;                //  lcd status intr requested
-  bool Timer_Overflow_IRQ = false;             //  timer overflowed intr requested
-  bool Serial_Transfer_Completion_IRQ = false; //  serial completion intr requested
-  bool Button_Pressed_IRQ = false;             //  button pressed intr requested
+  bool Button_Pressed_IRQ = false;             //  button pressed intr requested, bit4
+  bool Serial_Transfer_Completion_IRQ = false; //  serial completion intr requested, bit3
+  bool Timer_Overflow_IRQ = false;             //  timer overflowed intr requested, bit2
+  bool LCDC_Status_IRQ = false;                //  lcd status intr requested, bit1
+  bool VBlank_IRQ = false;                     //  vertical blanking intr requested, bit0
 
-  bool VBlank_Enabled = false;                     //  vertical blanking intr enabled
-  bool LCDC_Status_Enabled = false;                //  lcd status intr enabled
-  bool Timer_Overflow_Enabled = false;             //  timer overflowed intr enabled
-  bool Serial_Transfer_Completion_Enabled = false; //  serial completion intr enabled
-  bool Button_Pressed_Enabled = false;             //  button pressed intr enabled
+  bool Button_Pressed_Enabled = false;             //  button pressed intr enabled, bit4
+  bool Serial_Transfer_Completion_Enabled = false; //  serial completion intr enabled, bit3
+  bool Timer_Overflow_Enabled = false;             //  timer overflowed intr enabled, bit2
+  bool LCDC_Status_Enabled = false;                //  lcd status intr enabled, bit1
+  bool VBlank_Enabled = false;                     //  vertical blanking intr enabled, bit0
 };
 
 }
--- src/io/interrupts/interrupts.cpp
+++ src/io/interrupts/interrupts.cpp
@@ -6,61 +6,43 @@ namespace tmbl {
 
 byte interrupts::read(const std::size_t index) const noexcept {
   switch (index) {
-    case 0xFF0F: // IF, interrupt request address
-             return (VBlank_IRQ << 4) | 
-               (LCDC_Status_IRQ << 3) | 
-            (Timer_Overflow_IRQ << 2) |
-(Serial_Transfer_Completion_IRQ << 1) | 
-                  Button_Pressed_IRQ;
-
-    case 0xFFFF: // IE, interrupt enable address
-             return (VBlank_Enabled << 4) | 
-	       (LCDC_Status_Enabled << 3) | 
-            (Timer_Overflow_Enabled << 2) |
-(Serial_Transfer_Completion_Enabled << 1) | 
-                  Button_Pressed_Enabled;
-
-    default: break;
+    case 0xFF0F: // IF 
+     return Button_Pressed_IRQ << 4 |
+Serial_Transfer_Completion_IRQ << 3 |
+            Timer_Overflow_IRQ << 2 |
+               LCDC_Status_IRQ << 1 |
+                    VBlank_IRQ;
+    case 0xFFFF: // IE
+     return Button_Pressed_Enabled << 4 | 
+Serial_Transfer_Completion_Enabled << 3 | 
+            Timer_Overflow_Enabled << 2 | 
+               LCDC_Status_Enabled << 1 | 
+                    VBlank_Enabled;
   }
 }
 
 void interrupts::write(const std::size_t index, const byte val) noexcept {
   switch (index) {
     case 0xFF0F: // IF
-                    VBlank_IRQ = val & 0b000'0000;
-               LCDC_Status_IRQ = val & 0b000'0001;
-            Timer_Overflow_IRQ = val & 0b000'0010;
-Serial_Transfer_Completion_IRQ = val & 0b000'0011;
-            Button_Pressed_IRQ = val & 0b000'0100;
+            Button_Pressed_IRQ = val & 0b001'0000;
+Serial_Transfer_Completion_IRQ = val & 0b000'1000;
+            Timer_Overflow_IRQ = val & 0b000'0100;
+               LCDC_Status_IRQ = val & 0b000'0010;
+                    VBlank_IRQ = val & 0b000'0001;
       break;
 
     case 0xFFFF: // IE
-                    VBlank_Enabled = val & 0b000'0000;
-               LCDC_Status_Enabled = val & 0b000'0001;
-            Timer_Overflow_Enabled = val & 0b000'0010;
-Serial_Transfer_Completion_Enabled = val & 0b000'0011;
-            Button_Pressed_Enabled = val & 0b000'0100;
+            Button_Pressed_Enabled = val & 0b0001'0000;
+Serial_Transfer_Completion_Enabled = val & 0b0000'1000;
+            Timer_Overflow_Enabled = val & 0b0000'0100;
+               LCDC_Status_Enabled = val & 0b0000'0010;
+                    VBlank_Enabled = val & 0b0000'0001;
       break;
-
-    default: break;
   }
 }
 
-uint8 interrupts::IF() const noexcept {
-                 return (VBlank_IRQ << 4) | 
-                   (LCDC_Status_IRQ << 3) | 
-                (Timer_Overflow_IRQ << 2) |
-    (Serial_Transfer_Completion_IRQ << 1) | 
-                      Button_Pressed_IRQ;
-}
-
-uint8 interrupts::IE() const noexcept {
-             return (VBlank_Enabled << 4) | 
-               (LCDC_Status_Enabled << 3) | 
-            (Timer_Overflow_Enabled << 2) |
- (Serial_Transfer_Completion_Enabled<< 1) | 
-                 Button_Pressed_Enabled;
-}
+uint8 interrupts::IF() const noexcept { return read(0xFF0F); }
+uint8 interrupts::IE() const noexcept { return read(0xFFFF); }
 
 }
 
