217cd5923f028be017664117a516bffa8391c5db
Adem Budak <admbudk@gmail.com>
2021-05-18 20:57:07 +0300

    Change order of read/write member functions
    This makes it identical with other mbc types.

--- src/cartridge/type/mbc2.cpp
+++ src/cartridge/type/mbc2.cpp
@@ -8,28 +8,6 @@ mbc2::mbc2(std::vector<byte> &&rom) : m_rom(std::move(rom)) {
   m_xram.resize(512_B); // mbc2 type cartridges has only 512x4 bit of xram
 }
 
-void mbc2::write(const std::size_t index, const byte val) noexcept {
-  if (index >= 0x0000 && index <= 0x3FFF) { // ramg and romb registers
-    if (val & 0b1000'0000) {                // romb
-      romb = ((val & 0b0000'1111) == 0) ? 1 : (val & 0b0000'1111);
-    } else { // ramg
-      xram_access_enabled = (val & 0b0000'1111) == 0b1010;
-    }
-  }
-
-  else if (index >= 0xA000 && index <= 0xBFFF) { // RAM (read/'write')
-    if (xram_access_enabled) {
-      const byte data = val & 0b0000'1111; // mbc2 ram has only 4 bit of datum
-      m_xram.at(index % 512) = data;
-    } else {
-      (void)val;
-    }
-  }
-
-  else {
-  }
-}
-
 byte mbc2::read(const std::size_t index) const noexcept { // ROM Bank 0 (Read only)
   if (index >= 0x0000 && index <= 0x3FFF) {               // bank 0
     return m_rom.at(index);
@@ -52,6 +30,28 @@ byte mbc2::read(const std::size_t index) const noexcept { // ROM Bank 0 (Read on
 
   else {
     // things
+  }
+}
+
+void mbc2::write(const std::size_t index, const byte val) noexcept {
+  if (index >= 0x0000 && index <= 0x3FFF) { // ramg and romb registers
+    if (val & 0b1000'0000) {                // romb
+      romb = ((val & 0b0000'1111) == 0) ? 1 : (val & 0b0000'1111);
+    } else { // ramg
+      xram_access_enabled = (val & 0b0000'1111) == 0b1010;
+    }
+  }
+
+  else if (index >= 0xA000 && index <= 0xBFFF) { // RAM (read/'write')
+    if (xram_access_enabled) {
+      const byte data = val & 0b0000'1111; // mbc2 ram has only 4 bit of datum
+      m_xram.at(index % 512) = data;
+    } else {
+      (void)val;
+    }
+  }
+
+  else {
   }
 }
 
