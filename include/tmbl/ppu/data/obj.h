9dfb2ca92ce9a66626c384ac1fd5d738bd9de462
Adem Budak <admbudk@gmail.com>
2021-06-22 22:20:21 +0300

    Rewrite obj entry class

--- include/tmbl/ppu/data/obj.h
+++ include/tmbl/ppu/data/obj.h
@@ -3,7 +3,9 @@
 
 #include "tmbl/config.h"
 
-// https://gbdev.io/pandocs/#vram-sprite-attribute-table-oam
+// https://gbdev.io/pandocs/OAM.html#vram-sprite-attribute-table-oam
+//
+//
 // https://archive.org/details/GameBoyProgManVer1.1/page/n62/mode/1up
 
 namespace tmbl {
@@ -12,21 +14,25 @@ class obj {
 public:
   obj(const byte y, const byte x, const byte tile_num, const byte attrib);
 
+  uint8 x() const noexcept;
+  uint8 y() const noexcept;
+
+  bool priority() const noexcept;
+  bool yFlip() const noexcept;
+  bool xFlip() const noexcept;
+  uint8 dmgPalette() const noexcept;
+
+  uint8 vramBank() const noexcept;
+  uint8 cgbPalette() const noexcept;
+
 private:
   byte y_pos;
   byte x_pos;
 
   byte tile_number;
-
-  flag obj_bg_priority;
-  flag y_flip;
-  flag x_flip;
-  flag palette_number;
-  flag vram_bank;          // cgb only
-  byte cgb_palette_number; // cgb only
+  byte attribute;
 };
 
 }
 
 #endif
-
--- src/ppu/data/obj.cpp
+++ src/ppu/data/obj.cpp
@@ -3,15 +3,19 @@
 namespace tmbl {
 
 obj::obj(const byte y, const byte x, const byte tile_num, const byte attrib)
-    : y_pos(y), x_pos(x), tile_number(tile_num) {
-  // clang-format off
-  obj_bg_priority =    attrib & 0b1000'0000;
-  y_flip =             attrib & 0b0100'0000;
-  x_flip =             attrib & 0b0010'0000;
-  palette_number =     attrib & 0b0001'0000; // 0:obp0 1:obp1
-  vram_bank =          attrib & 0b0000'1000; // 0:bank0 1:bank1:
-  cgb_palette_number = attrib & 0b0000'0111; // obp0-7
-}
+    : y_pos(y), x_pos(x), tile_number(tile_num), attribute(attrib) {}
+// clang-format off
+
+uint8 obj::x() const noexcept          { return y_pos; }
+uint8 obj::y() const noexcept          { return x_pos; }
+
+bool obj::priority() const noexcept    { return attribute & 0b1000'0000; }
+bool obj::yFlip() const noexcept       { return attribute & 0b0100'0000; }
+bool obj::xFlip() const noexcept       { return attribute & 0b0010'0000; }
+uint8 obj::dmgPalette() const noexcept { return attribute & 0b0001'0000; }
+
+uint8 obj::vramBank() const noexcept   { return attribute & 0b0000'1000; } // cgb only
+uint8 obj::cgbPalette() const noexcept { return attribute & 0b0000'0111; } // cgb only
 
 }
 
