e98688a587f08ce69eddb5ec26e8628a96f67865
Adem Budak <admbudk@gmail.com>
2021-06-22 20:23:14 +0300

    Set behavior of HDMA1/4 registers on read

--- src/io/registers.cpp
+++ src/io/registers.cpp
@@ -9,14 +9,20 @@
 namespace tmbl {
 byte &registers::getAt(const std::size_t index) { return m_regs.at(index - memory::io); }
 
-byte registers::read(const std::size_t index) const noexcept { return m_regs.at(index); }
+byte registers::read(const std::size_t index) const noexcept {
+  // HDMA-1/2/3/4 registers returns 0xff on read
+  if (index == 0xFF51 || index == 0xFF52 || index == 0xFF53 || index == 0xFF54) {
+    return 0xFF;
+  } else {
+    return m_regs.at(index);
+  }
+}
 
 void registers::write(const std::size_t index, const byte val) noexcept {
-  if (index == 0xFF04) { // Writes to DIV, resets it to zero
+  if (index == 0xFF04) { // DIV register reset to zero on write
     m_regs.at(index) = 0;
   } else {
     m_regs.at(index) = val;
   }
 }
-
 }
